ad9f00d37cad0d73b8e707acb90da6726852f358
Victor-AGSP <109920615+Victor-AGSP@users.noreply.github.com>
2024-09-21 19:10:48 -0300

    Agregar

---
+++ proyecto/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cards from "./components/Cards";
+import News from "./components/News";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Hero />
+                <Cards />
+                <News />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
