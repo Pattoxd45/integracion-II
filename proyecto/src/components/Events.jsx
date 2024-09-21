9829822c4e28d1680ed85fa28bd4915811ae1d15
NatalyHSaez <112361725+NatalyHSaez@users.noreply.github.com>
2024-09-21 16:25:21 -0300

    modificacion diseño eventos

--- proyecto/src/components/Events.jsx
+++ proyecto/src/components/Events.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axios from "axios";
 
-const EventsCarousel = () => {
+const Events = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -21,7 +21,7 @@ const EventsCarousel = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3, // Mostrar 3 eventos a la vez
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -33,8 +33,9 @@ const EventsCarousel = () => {
         {events.map((event, index) => (
           <div 
             key={index} 
-            className="w-[calc(33.33%-1rem)] bg-[#000] rounded-lg overflow-hidden h-[200px] shadow-xl flex flex-col items-center justify-center mx-2 border border-black"
+            className="relative w-[calc(33.33%-1rem)] bg-black overflow-hidden h-[200px] shadow-xl flex flex-col items-center justify-center mx-2 border border-black"
           >
+            <div className="absolute inset-y-0 left-0 w-1 bg-gradient-to-b from-orange-500 to-transparent pointer-events-none"></div>
             <div className="flex flex-col items-center justify-center h-full text-center">
               <h2 className="text-xl font-bold">{event.title}</h2>
               <p className="mt-1">{event.date}</p>
@@ -47,4 +48,4 @@ const EventsCarousel = () => {
   );
 };
 
-export default EventsCarousel;
+export default Events;
