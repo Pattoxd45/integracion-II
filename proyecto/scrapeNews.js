8cf5434cd3c09fddc8aac5354b8060264020b51d
NatalyHSaez <112361725+NatalyHSaez@users.noreply.github.com>
2024-09-28 16:14:39 -0300

    scaping noticias

--- proyecto/scrapeNews.js
+++ proyecto/scrapeNews.js
@@ -8,18 +8,25 @@ async function scrapeNews() {
     const articles = [];
 
     $('article.css-415ug').each((index, element) => {
-        const title = $(element).find('h3.css-9f4rq').text();
-        const description = $(element).find('div.css-p4BJO p').text();
+        const title = $(element).find('h3.css-9f4rq').text().trim();
+        const description = $(element).find('div.css-p4BJO p').text().trim();
         const imageUrl = $(element).find('picture source').attr('srcset');
+        const authorName = $(element).find('.css-Z5ZSx').text().trim();
+        const authorImageUrl = $(element).find('.css-l31Oj img').attr('src');
+        const category = $(element).find('.css-6ZZbL a').text().trim();
 
         articles.push({
             title,
             description,
-            imageUrl,
+            imageUrl: imageUrl ? `https:${imageUrl}` : null, // Añadir el prefijo https si existe
+            author: authorName,
+            authorImageUrl: authorImageUrl ? `https:${authorImageUrl}` : null, // Añadir el prefijo https si existe
+            category,
         });
     });
 
+    console.log(articles)
     return articles;
 }
 
-module.exports = scrapeNews; 
+module.exports = scrapeNews;
