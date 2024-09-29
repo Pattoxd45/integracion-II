1ec6204373bed6a3406901d67fb033cd9d6bde4f
Ocofer-silva <Cristophersilvaysusdibujosxd2@gmail.com>
2024-09-29 03:43:48 -0300

    componentes nuevos
    Minijuegos
    Tiendas

---
+++ proyecto/src/components/Tiendas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tiendas = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Tiendas</h1>
+      <p>Aquí puedes encontrar información sobre nuestras tiendas.</p>
+    </div>
+  );
+};
+
+export default Tiendas;
