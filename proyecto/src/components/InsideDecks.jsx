eaa25d625b2397d99a1d976a830a0b5875be8ee3
Marcelo Santana <132941573+cchelooo@users.noreply.github.com>
2024-10-30 22:27:16 -0300

    vistas dentro de la baraja

--- proyecto/src/components/InsideDecks.jsx
+++ proyecto/src/components/InsideDecks.jsx
@@ -1,127 +1,114 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { AiOutlineClose } from "react-icons/ai";
-import { IoIosOptions, IoIosAdd } from "react-icons/io";
-import { MdEdit, MdEditOff } from "react-icons/md";
-import { FaTrashCan } from "react-icons/fa6";
-import InsideDecksProperties from "./InsideDecksProperties"; // Importamos InsideDecksProperties
+import { MdEdit } from "react-icons/md";
+import { BiArrowBack } from "react-icons/bi";
+import ScaleLoader from "react-spinners/ScaleLoader";
+import InsideDecksProperties from "./InsideDecksProperties";
 
 const InsideDecks = ({ closeModal, deckName, deckId }) => {
-  const navigate = useNavigate(); // Usamos el hook useNavigate para navegar
   const modalRef = useRef(null);
 
-  // Estado para almacenar las cartas en el mazo (inicialmente vacío)
-  const [cards, setCards] = useState([]); 
+  const [cards, setCards] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredCards, setFilteredCards] = useState(cards);
+  const [filteredCards, setFilteredCards] = useState([]);
   const [editMode, setEditMode] = useState(false);
-  const [selectedCards, setSelectedCards] = useState([]);
-  const [selectedCardDetails, setSelectedCardDetails] = useState(null);
-  const [activeView, setActiveView] = useState("Cartas"); // Estado para controlar la vista activa (Cartas o Propiedades)
+  const [activeView, setActiveView] = useState("Cartas");
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedCard, setSelectedCard] = useState(null);
 
-  // Llamada a la API para obtener las cartas del mazo
-  useEffect(() => {
-    const fetchDeckCards = async () => {
-      try {
-        const response = await fetch(`https://magicarduct.online:3000/api/mazocartas/${deckId}`);
-        if (response.ok) {
-          const data = await response.json();
-          setCards(data); // Almacenar las cartas del mazo en el estado
-        } else {
-          console.error("Error al obtener las cartas del mazo");
-        }
-      } catch (error) {
-        console.error("Error en la petición:", error);
+  const fetchDeckCards = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`https://magicarduct.online:3000/api/mazocartas/${deckId}`);
+      if (!response.ok) {
+        throw new Error("Error al obtener las cartas del mazo");
       }
-    };
+      const deckCards = await response.json();
+      const deckCardsData = [];
 
+      for (const card of deckCards) {
+        const cardId = card.IDcarta;
+        const cardResponse = await fetch(`https://api.scryfall.com/cards/${cardId}`);
+        if (cardResponse.ok) {
+          const cardData = await cardResponse.json();
+          deckCardsData.push({
+            id: cardData.id,
+            name: cardData.name,
+            imageUrl: cardData.image_uris?.png || "https://via.placeholder.com/150",
+            mana_cost: cardData.mana_cost,
+            oracle_text: cardData.oracle_text,
+            cmc: cardData.cmc,
+            type_line: cardData.type_line,
+            colors: cardData.colors,
+            lang: "en"
+          });
+        } else {
+          console.error(`Error al obtener la carta con ID ${cardId}`);
+        }
+      }
+
+      setCards(deckCardsData);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error en la petición:", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchDeckCards();
   }, [deckId]);
 
   useEffect(() => {
-    setFilteredCards(cards); // Actualizar las cartas filtradas cuando se actualiza el estado de las cartas
+    setFilteredCards(cards);
   }, [cards]);
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
-        closeModal();
-      }
-    };
-
-    const handleEscKey = (event) => {
-      if (event.key === "Escape") {
-        closeModal();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("keydown", handleEscKey);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.removeEventListener("keydown", handleEscKey);
-    };
-  }, [modalRef, closeModal]);
-
-  useEffect(() => {
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
     const filtered = cards.filter((card) =>
-      card.name.toLowerCase().includes(searchQuery.toLowerCase())
+      card.name.toLowerCase().includes(e.target.value.toLowerCase())
     );
     setFilteredCards(filtered);
-  }, [searchQuery, cards]);
-
-  const toggleEditMode = () => {
-    setEditMode(!editMode);
-    setSelectedCards([]);
-    setSelectedCardDetails(null); // Cerrar detalles de carta si se activa el modo edición
-  };
-
-  const toggleSelectCard = (cardId) => {
-    if (selectedCards.includes(cardId)) {
-      setSelectedCards(selectedCards.filter((id) => id !== cardId));
-    } else {
-      setSelectedCards([...selectedCards, cardId]);
-    }
-  };
-
-  const handleDelete = () => {
-    const remainingCards = filteredCards.filter(
-      (card) => !selectedCards.includes(card.id)
-    );
-    setFilteredCards(remainingCards);
-    setSelectedCards([]);
   };
 
   const handleCardClick = (card) => {
-    if (!editMode) {
-      setSelectedCardDetails(card);
-    }
+    setSelectedCard(card);
   };
 
   const closeCardDetails = () => {
-    setSelectedCardDetails(null); // Cerrar detalles de carta
+    setSelectedCard(null);
   };
 
-  // Función para navegar a la página Cartas.jsx
-  const handleAddCardClick = () => {
-    navigate("/cartas", { state: { addingCards: true } }); // Navegar a Cartas con la marca de añadir cartas
+  // Función para cerrar el modal al hacer clic fuera de él
+  const handleOutsideClick = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      closeModal();
+    }
   };
+
+  // Función para cerrar el modal al presionar la tecla Escape
+  const handleEscapeKey = (e) => {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, []);
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-70 z-50 flex justify-center items-center">
+    <div className="fixed inset-0 z-50 flex justify-center items-center">
       <div
         ref={modalRef}
-        className="relative w-[1150px] h-[95vh] max-h-[95vh] p-5 bg-[#0b0f14] rounded-lg border-[2px] border-[rgba(255,255,255,0.1)] border-opacity-65 overflow-hidden flex m-auto"
+        className="relative w-[1250px] h-[95vh] max-h-[95vh] p-5 bg-[#0b0f14] rounded-lg border-[1px] border-[rgba(255,255,255,0.1)] overflow-hidden flex m-auto"
       >
-        {/* Botón de cerrar */}
         <button
           className="absolute top-2 right-2 text-[#e1e6ea] hover:opacity-70 transition"
           onClick={closeModal}
@@ -129,11 +116,11 @@ const InsideDecks = ({ closeModal, deckName, deckId }) => {
           <AiOutlineClose size={24} />
         </button>
 
-        {/* Contenedor de cartas y detalles */}
-        <div className={`relative w-${selectedCardDetails ? "[35%]" : "[100%]"} transition-all duration-500`}>
-          <h2 className="text-[#e1e6ea] text-2xl font-bold mb-4">{deckName}</h2>
+        <div className={`relative transition-all duration-500 ${selectedCard ? "w-[28%] mr-6" : "w-full"}`}>
+          <h2 className="text-[#e1e6ea] text-2xl font-bold mb-4">
+            {deckName}
+          </h2>
 
-          {/* Botones "Cartas" y "Propiedades" */}
           <div className="flex justify-between space-x-2 mb-4">
             <button
               className={`w-full h-[46px] rounded-md text-lg font-semibold text-center ${
@@ -153,111 +140,65 @@ const InsideDecks = ({ closeModal, deckName, deckId }) => {
             </button>
           </div>
 
-          {/* Contenido de Cartas o Propiedades */}
           {activeView === "Cartas" ? (
             <>
-              {/* Barra de búsqueda, opciones y botón de editar */}
               <div className="flex items-center space-x-3 mb-6">
                 <input
                   type="text"
                   value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
+                  onChange={handleSearchChange}
                   placeholder="Buscar cartas..."
-                  className="w-full h-[46px] px-4 text-black bg-white rounded-md focus:outline-none"
+                  className="flex-grow h-[46px] px-4 text-black bg-white rounded-md focus:outline-none min-w-[200px]"
                 />
                 <button
-                  className="w-[91px] h-[46px] bg-[#2a5880] text-[#e1e6ea] font-semibold rounded-md text-center hover:opacity-80 transition"
+                  className="h-[46px] bg-[#2a5880] text-[#e1e6ea] font-semibold rounded-md text-center hover:opacity-80 transition"
+                  style={{ width: "91px" }}
                 >
                   Buscar
                 </button>
                 <button
-                  className="bg-[#2a5880] text-[#e1e6ea] rounded-md p-[10px] hover:opacity-80 transition flex items-center justify-center"
-                  style={{ width: "46px", height: "46px" }}
+                  className="w-[46px] h-[46px] bg-[#2a5880] text-[#e1e6ea] font-semibold rounded-md text-center hover:opacity-80 transition flex items-center justify-center"
                 >
-                  <IoIosOptions className="text-[#e1e6ea] text-[24px]" />
-                </button>
-                <button
-                  className={`rounded-md p-[10px] transition-colors flex items-center justify-center ${
-                    editMode
-                      ? "bg-[#9ebbd6] hover:bg-[#93aec7]"
-                      : "bg-[#2a5880] hover:bg-[#275379]"
-                  }`}
-                  style={{ width: "46px", height: "46px" }}
-                  onClick={toggleEditMode}
-                >
-                  {editMode ? (
-                    <MdEditOff className="text-[#e1e6ea] text-[24px]" />
-                  ) : (
-                    <MdEdit className="text-[#e1e6ea] text-[24px]" />
-                  )}
+                  <MdEdit size={20} />
                 </button>
               </div>
 
-              {/* Contenedor de cartas con scroll */}
-              <div className="overflow-y-auto max-h-[68vh] pr-4 rounded-md">
-                <div className="grid grid-cols-1 gap-4">
-                  {filteredCards.length > 0 ? (
-                    filteredCards.map((card) => (
-                      <div
-                        key={card.id}
-                        className="relative flex bg-[#333] p-3 rounded-md cursor-pointer"
-                        onClick={() => handleCardClick(card)}
-                      >
-                        {/* Imagen a la izquierda */}
-                        <div className="w-[150px] flex-shrink-0">
+              <div className="overflow-y-auto h-[80%] rounded-md custom-scrollbar mb-4">
+                {isLoading ? (
+                  <div className="flex justify-center items-center h-full">
+                    <ScaleLoader color="#e1e6ea" height={35} />
+                  </div>
+                ) : (
+                  <div className="flex flex-col space-y-3">
+                    {filteredCards.length > 0 ? (
+                      filteredCards.map((card) => (
+                        <div
+                          key={card.id}
+                          onClick={() => handleCardClick(card)}
+                          className="mr-[6px] flex items-center bg-[#12171e] rounded-lg cursor-pointer"
+                          style={{ height: "132px" }}
+                        >
                           <img
-                            src={card.image}
+                            src={card.imageUrl}
                             alt={card.name}
-                            className="w-full h-[120px] object-cover"
+                            className="object-cover rounded-none"
+                            style={{ width: "165px", height: "100%", objectPosition: "top" }}
                           />
+                          <h4
+                            className={`text-[#e1e6ea] text-lg ml-4 ${selectedCard ? "truncate" : ""}`}
+                            style={{ maxWidth: selectedCard ? "120px" : "auto" }}
+                          >
+                            {card.name}
+                          </h4>
                         </div>
+                      ))
+                    ) : (
+                      <p className="text-[#e1e6ea]">No se han encontrado cartas en este mazo.</p>
+                    )}
 
-                        {/* Contenido a la derecha */}
-                        <div className="ml-4 flex-grow">
-                          <h3 className="text-[#e1e6ea] text-xl font-bold">{card.name}</h3>
-                          <p className="text-gray-400 mt-2">{card.type}</p>
-                        </div>
-
-                        {/* Circulo de selección */}
-                        {editMode && (
-                          <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
-                            <div
-                              className={`relative w-5 h-5 rounded-full border-2 ${
-                                selectedCards.includes(card.id)
-                                  ? "bg-[#2a5880] border-[#2a5880]"
-                                  : "bg-white border-gray-300"
-                              }`}
-                              onClick={() => toggleSelectCard(card.id)}
-                            ></div>
-                          </div>
-                        )}
-                      </div>
-                    ))
-                  ) : (
-                    <p className="text-[#e1e6ea]">No se han agregado cartas a este mazo.</p>
-                  )}
-                </div>
-              </div>
-
-              {/* Botones de añadir y eliminar cartas */}
-              <div className="flex justify-end space-x-4 mt-4">
-                {selectedCards.length > 0 && (
-                  <button
-                    onClick={handleDelete}
-                    className="bg-[#2a5880] text-[#ddd] rounded-full hover:bg-[#b52e0e] transition-colors p-3 flex items-center justify-center"
-                    style={{ width: "46px", height: "46px" }}
-                  >
-                    <FaTrashCan className="text-[20px]" />
-                  </button>
+                    <div className="h-4" />
+                  </div>
                 )}
-
-                <button
-                  onClick={handleAddCardClick} // Navega a Cartas.jsx
-                  className="bg-[#2a5880] text-[#ddd] rounded-full hover:opacity-80 transition p-3 flex items-center justify-center"
-                  style={{ width: "46px", height: "46px" }}
-                >
-                  <IoIosAdd className="text-[20px]" />
-                </button>
               </div>
             </>
           ) : (
@@ -265,40 +206,47 @@ const InsideDecks = ({ closeModal, deckName, deckId }) => {
           )}
         </div>
 
-        {/* Detalles de la carta seleccionada */}
-        {selectedCardDetails && (
-          <div className="w-[65%] bg-[#222] ml-4 p-5 rounded-lg text-[#e1e6ea] flex">
-            {/* Imagen de la carta */}
-            <div className="w-[50%] flex-shrink-0">
+        {/* Popup de detalles de la carta con ajustes de margen y alineación */}
+        {selectedCard && (
+          <div className="absolute top-1/2 right-4 transform -translate-y-1/2 w-[68.25%] h-[93%] bg-[#12171E] p-6 text-[#e1e6ea] overflow-y-auto flex rounded-lg mr-4 custom-scrollbar">
+            <button
+              onClick={closeCardDetails}
+              className="absolute top-2 left-6 text-[#e1e6ea] hover:text-red-500 transition"
+            >
+              <BiArrowBack size={24} />
+            </button>
+            <div className="flex items-center w-full justify-center">
               <img
-                src={selectedCardDetails.image}
-                alt={selectedCardDetails.name}
-                className="w-full h-full object-cover rounded-md"
+                src={selectedCard.imageUrl}
+                alt={selectedCard.name}
+                className="w-[500px] h-auto object-cover mr-1"
               />
-            </div>
-
-            {/* Descripciones de la carta */}
-            <div className="ml-6 w-[50%] flex flex-col">
-              <button
-                className="self-end text-[#e1e6ea] hover:text-red-500 mb-4"
-                onClick={closeCardDetails}
-              >
-                Cerrar
-              </button>
-              <h2 className="text-3xl font-bold">{selectedCardDetails.name}</h2>
-              <p className="mt-4 text-lg">
-                Descripción: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-              <p className="mt-2">Mana: {selectedCardDetails.mana || "Desconocido"}</p>
-              <p className="mt-2">Tipo: {selectedCardDetails.type || "Desconocido"}</p>
-              <p className="mt-2">
-                Poder/Toughness: {selectedCardDetails.power || "N/A"} /{" "}
-                {selectedCardDetails.toughness || "N/A"}
-              </p>
+              <div className="ml-4 flex flex-col">
+                <h2 className="text-2xl font-bold mb-2">{selectedCard.name}</h2>
+                <p className="text-sm mb-2"><strong>Language:</strong> {selectedCard.lang}</p>
+                <p className="text-sm mb-2"><strong>Mana Cost:</strong> {selectedCard.mana_cost}</p>
+                <p className="text-sm mb-2"><strong>Oracle Text:</strong> {selectedCard.oracle_text}</p>
+                <p className="text-sm mb-2"><strong>CMC:</strong> {selectedCard.cmc}</p>
+                <p className="text-sm mb-2"><strong>Type Line:</strong> {selectedCard.type_line}</p>
+                <p className="text-sm mb-2"><strong>Colors:</strong> {selectedCard.colors.join(", ")}</p>
+              </div>
             </div>
           </div>
         )}
       </div>
+      <style jsx>{`
+        /* Estilo del scrollbar */
+        .custom-scrollbar::-webkit-scrollbar {
+          width: 4px;
+        }
+        .custom-scrollbar::-webkit-scrollbar-thumb {
+          background-color: rgba(255,255,255,0.1);
+          border-radius: 10px;
+        }
+        .custom-scrollbar::-webkit-scrollbar-track {
+          background: transparent;
+        }
+      `}</style>
     </div>
   );
 };
