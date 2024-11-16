c6491796cc368a061b764eb3a779450e8477e52b
Pattoxd45 <valdespatricio832@gmail.com>
2024-11-15 22:08:52 -0300

    fail

--- proyecto/src/components/FilterSection.jsx
+++ proyecto/src/components/FilterSection.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 
-const FilterSection = ({
-  showFilters,
-  toggleFilters,
-  filter,
-  handleFilterChange,
-  handleKeywordAbilityChange,
-  handleColorsChange,
-  keywordAbilities,
-  sets,
-  subtypes,
-}) => {
+const FilterSection = ({ showFilters, toggleFilters, filter, handleFilterChange, handleColorsChange, sets, subtypes }) => {
   return (
     <div>
-      <button
-        onClick={toggleFilters}
+      <button 
+        onClick={toggleFilters} 
         className="bg-blue-500 text-white rounded p-2 mb-4"
       >
         {showFilters ? 'Ocultar Filtros' : 'Filtros'}
@@ -22,7 +12,6 @@ const FilterSection = ({
 
       {showFilters && (
         <div>
-          {/* Filtros de colores */}
           <div className="mb-4 flex flex-wrap">
             <label className="text-white mr-4">Colores:</label>
             {['White', 'Blue', 'Black', 'Red', 'Green'].map((color) => (
@@ -38,26 +27,6 @@ const FilterSection = ({
             ))}
           </div>
 
-          {/* Filtros de habilidades clave */}
-          <div className="flex flex-col mb-4">
-            <label className="text-white mb-1">Habilidades Clave:</label>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-              {keywordAbilities.map((ability, index) => (
-                <div key={index} className="flex items-center">
-                  <input
-                    type="checkbox"
-                    value={ability}
-                    onChange={handleKeywordAbilityChange}
-                    className="mr-2"
-                    checked={filter.keywordAbilities.includes(ability)} // Marca si la habilidad está seleccionada
-                  />
-                  <span className="text-white">{ability}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          {/* Filtros numéricos (CDM, Poder, Resistencia) */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-4">
             <div className="flex flex-col">
               <label className="text-white mb-1">CDM:</label>
@@ -93,7 +62,6 @@ const FilterSection = ({
             </div>
           </div>
 
-          {/* Filtros de Tipo, Edición y Subtipo */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 mb-4">
             <div className="flex flex-col">
               <label className="text-white mb-1">Tipo:</label>
