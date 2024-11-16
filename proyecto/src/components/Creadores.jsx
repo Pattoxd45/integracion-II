a28b7d3a268f5297896f41d03d3d3f3481597146
NatalyHSaez <112361725+NatalyHSaez@users.noreply.github.com>
2024-11-16 07:42:32 -0300

    Monitoreo Creadores

--- proyecto/src/components/Creadores.jsx
+++ proyecto/src/components/Creadores.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -16,6 +16,8 @@ import imagen9 from "../images/imgCreadores/imagen_9.jpg";
 import imagen10 from "../images/imgCreadores/imagen_10.jpg";
 
 const Creadores = () => {
+  const [loading, setLoading] = useState(true); // Estado inicial como "cargando"
+
   const settings = {
     dots: true,
     infinite: true,
@@ -25,24 +27,9 @@ const Creadores = () => {
     autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3, // Muestra 3 en pantallas grandes
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2, // Muestra 2 en tablets
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1, // Muestra 1 en móviles
-        },
-      },
+      { breakpoint: 1024, settings: { slidesToShow: 3 } },
+      { breakpoint: 768, settings: { slidesToShow: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 1 } },
     ],
   };
 
@@ -59,30 +46,49 @@ const Creadores = () => {
     { name: "Luis Scott-Vargas", imgSrc: imagen10, link: "https://www.youtube.com/@LSVargas", description: "Comparte experiencias y estrategias de MTG" },
   ];
 
+  useEffect(() => {
+    // Temporizador de 5 segundos
+    const timer = setTimeout(() => {
+      setLoading(false); // Cambia el estado a "no cargando"
+    }, 2000);
+
+    return () => clearTimeout(timer); // Limpia el temporizador si el componente se desmonta
+  }, []);
+
   return (
     <div className="max-w-[1200px] mx-auto my-6 flex items-center">
       <div className="p-10 rounded-lg text-white w-full">
-        <Slider {...settings}>
-          {creators.map((creator, index) => (
-            <div key={index} className="flex justify-center">
-              <a href={creator.link} target="_blank" rel="noopener noreferrer">
-                <div className="card w-full max-w-[220px] bg-[#000] flex flex-col rounded-lg border-4 border-[#2a5880] shadow-xl transition-all duration-300 hover:shadow-[0_0_30px_rgba(45,89,128,1)]">
-                  <div className="relative w-full aspect-[4/3]">
-                    <img
-                      src={creator.imgSrc}
-                      alt={creator.name}
-                      className="absolute inset-0 w-full h-full object-cover border-b-4 border-[#2a5880] rounded-t-lg"
-                    />
+        {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <div className="w-8 h-8 border-4 border-[#e2e7eb] border-t-transparent rounded-full animate-spin"> </div>
+        </div>
+        ) : creators.length === 0 ? (
+          <div className="bg-gradient-to-b from-[#12171E] via-[#222b38] to-[#222e3f] text-[#e2e7eb] text-center p-4 rounded-lg">
+            No hay eventos disponibles.
+          </div>
+        ) : (
+          <Slider {...settings}>
+            {creators.map((creator, index) => (
+              <div key={index} className="flex justify-center">
+                <a href={creator.link} target="_blank" rel="noopener noreferrer">
+                  <div className="card w-full max-w-[220px] bg-[#000] flex flex-col rounded-lg border-4 border-[#2a5880] shadow-xl transition-all duration-300 hover:shadow-[0_0_30px_rgba(45,89,128,1)]">
+                    <div className="relative w-full aspect-[4/3]">
+                      <img
+                        src={creator.imgSrc}
+                        alt={creator.name}
+                        className="absolute inset-0 w-full h-full object-cover border-b-4 border-[#2a5880] rounded-t-lg"
+                      />
+                    </div>
+                    <div className="flex-1 flex flex-col justify-center text-center p-2">
+                      <div className="font-bold text-[#FF7F50]">{creator.name}</div>
+                      <div className="text-[#FFDAB9]">{creator.description}</div>
+                    </div>
                   </div>
-                  <div className="flex-1 flex flex-col justify-center text-center p-2">
-                    <div className="font-bold text-[#FF7F50]">{creator.name}</div>
-                    <div className="text-[#FFDAB9]">{creator.description}</div>
-                  </div>
-                </div>
-              </a>
-            </div>
-          ))}
-        </Slider>
+                </a>
+              </div>
+            ))}
+          </Slider>
+        )}
       </div>
     </div>
   );
